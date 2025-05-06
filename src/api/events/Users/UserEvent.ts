e2c218b87daf8f37668864fcae592d369f3f9240
KhushagraSikka <khushagrasikka@gmail.com>
2025-05-06 20:23:40 +0000

    Code cleanup and optimization

---
+++ src/api/events/Users/UserEvent.ts
@@ -0,0 +1,18 @@
+import { EventSubscriber, On } from 'event-dispatch';
+import { SendWelcomeMail } from '@api/queue-jobs/Users/SendWelcomeMail';
+
+@EventSubscriber()
+export class UserEvent {
+  @On('onUserRegister')
+  public onUserRegister(user: any) {
+    new SendWelcomeMail(user).setOptions({ delay: 5000 }).dispatch();
+  }
+
+  @On('onUserCreate')
+  public onUserCreate(user: any) {
+    console.log('User ' + user.email + ' created!');
+  }
+}
+
+
+// Potential improvement: Add error handling
